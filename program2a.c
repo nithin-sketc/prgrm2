b768a3a0d63196da4bae3806fd3ec6a0e4f01c45
nithin-sketc <nithinr.cs24@bmsce.ac.in>
2025-11-09 19:21:34 +0530

    Create program2a.c

---
+++ program2a.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#define MAX 5 // maximum size of the queue
+
+int queue[MAX];
+int front = -1, rear = -1;
+
+// Function to insert an element in the queue
+void insert(int value)
+{
+if (rear == MAX - 1)
+{
+printf("Queue Overflow! Cannot insert %d\n", value);
+}
+else
+{
+if (front == -1)
+{
+front = 0; // first insertion
+}
+rear++;
+queue[rear] = value;
+printf("%d inserted into the queue.\n", value);
+}
+}
+
+// Function to delete an element from the queue
+void delete()
+{
+if (front == -1 || front > rear)
+
+{
+printf("Queue Underflow! Queue is empty.\n");
+}
+else
+{
+printf("Deleted element: %d\n", queue[front]);
+front++;
+}
+}
+
+// Function to display the elements of the queue
+void display()
+{
+if (front == -1 || front > rear)
+{
+printf("Queue is empty.\n");
+}
+else
+{
+printf("Queue elements: ");
+for (int i = front; i <= rear; i++)
+{
+printf("%d ", queue[i]);
+}
+printf("\n");
+}
+}
+
+int main()
+{
+int choice, value;
+
+while (1)
+{
+printf("\nQueue Operations:\n");
+printf("1. Insert\n");
+printf("2. Delete\n");
+printf("3. Display\n");
+printf("4. Exit\n");
+printf("Enter your choice: ");
+scanf("%d", &choice);
+
+switch (choice)
+{
+case 1:
+printf("Enter value to insert: ");
+scanf("%d", &value);
+insert(value);
+break;
+case 2:
+delete();
+break;
+case 3:
+display();
+break;
+case 4:
+printf("Exiting program.\n");
+return 0;
+default:
+printf("Invalid choice! Please try again.\n");
+}
+}
+return 0;
+}
